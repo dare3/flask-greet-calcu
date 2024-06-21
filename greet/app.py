8d3d8f5a0bee53d0642ac5d986fd1a383778cfc0
Dare3 <enitan_segun@yahoo.co.uk>
2024-06-21 16:15:23 -0400

    re-modified code

--- greet/app.py
+++ greet/app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def greet():
     return "<h1>WELCOME</h1>"
 
-@app.route("/welcome/<home>")
-def welcome(home):
-    return f"<h1> Welcome, {home}!</h1>"
+@app.route("/welcome/home")
+def welcome():
+    return "<h1> Welcome, home!</h1>"
 
-@app.route("/welcome/<back>")
-def welcome_back(back):
-    return f"<h1> Welcome, {back}~~~</h1>"
+@app.route("/welcome/back")
+def welcome_back():
+    return "<h1> Welcome, back~~~</h1>"
